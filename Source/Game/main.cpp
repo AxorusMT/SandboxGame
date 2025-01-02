e9a40bd1c905c88fbbc2d21cc2ff5392058262b0
AxorusMT <ftmwini@gmail.com>
2025-01-02 15:09:32 +0000

    feat: initial switch to raylib

--- Source/Game/main.cpp
+++ Source/Game/main.cpp
@@ -1,408 +1,92 @@
-#pragma check_stack(off)
-
-#define SOKOL_IMPL
-#define SOKOL_GLCORE // using opengl
-#define FONTSTASH_IMPLEMENTATION
-#define SOKOL_IMGUI_NO_SOKOL_APP 
-#define STB_IMAGE_IMPLEMENTATION
-#include <cstdio>
-#include <cstdlib>
-#include "fontstash/fontstash.h"
-#include "sokol/sokol_gfx.h"
-#include "sokol/util/sokol_gl.h"
-#include "sokol/util/sokol_fontstash.h"
-#include "sokol/sokol_fetch.h"
-#include "stb_image.h"
-
-//#include <imgui.h>
-//#include "sokol/util/sokol_imgui.h"
-
-#include <print>
+#include <raylib.h>
 #include <string>
+#include <format>
+#include <vector>
 #include <cmath>
-#include <cstdlib>
-#include <GLFW/glfw3.h>
-
-void basicLogger(const char* tag, uint32_t log_level, uint32_t log_item_id, const char* message, uint32_t line, const char* filename, void* data) {
-    std::string level;
-
-    switch (log_level) {
-        case 0:
-            level = "panic";
-            break;
-        case 1:
-            level = "error";
-            break;
-        case 2:
-            level = "warning";
-            break;
-        case 3:
-            level = "info";
-            break;
-        default:
-            level = "unknown";
-    }
-
-    std::println("{} ({}): {} @ {}:{} ({}) {}",
-        level,
-        tag,
-        message,
-        filename,
-        line,
-        log_item_id,
-        data
-    );
-}
-
-sg_image tex = {};
-
-void image_callback(const sfetch_response_t* response) {
-    if (response->fetched) {
-        int pngwidth, pngheight, pngchannels;
-        constexpr int idealchannels = 4;
-        stbi_uc* pixels = stbi_load_from_memory(
-            (stbi_uc*)response->data.ptr,
-            (int)response->data.size, 
-            &pngwidth,
-            &pngheight,
-            &pngchannels,
-            idealchannels
-        );
-
-        if (pixels) {
-            std::println("image was loaded!");
-            sg_image_desc loaded_image_desc = {
-                .width = pngwidth,
-                .height = pngheight,
-                .pixel_format = SG_PIXELFORMAT_RGBA8, // Change this for some wackiness!
-            };
-
-            loaded_image_desc.data.subimage[0][0] = {
-                .ptr = pixels,
-                .size = (size_t)(pngwidth * pngheight * 4) // i guess w * h * 4 (rgba) per pixel?
-            };
-
-            tex = sg_make_image(loaded_image_desc);
-
-            stbi_image_free(pixels);
-        }
-    } else {
-        std::println("response failed!");
-    }
-}
-
-FONScontext* fons;
-float dpi = 96.0f; // TODO: Make this actually find the dpi, glfw has some functions for it
-int font;
-uint8_t* font_data = (uint8_t*) std::malloc(256 * 1024);
-// sg_bindings bindings = {};
-
-const char* fileutil_get_path(const char* filename, char* buf, size_t buf_size) {
-    snprintf(buf, buf_size, "%s", filename);
-    return buf;
-}
-
-void onFontLoad(const sfetch_response_t* response) {
-    if (response->fetched) {
-        std::println("fetch successful");
-        font = fonsAddFontMem(fons, "sans", (unsigned char*)response->data.ptr, (int)response->data.size, false);
-    } else {
-        std::println("{}", static_cast<int>(response->error_code));
-    }
-}
+#include <algorithm>
+#include <print>
 
 int main() {
-    constexpr int width = 1280;
-    constexpr int height = 720;
+    constexpr int screenWidth = 1280;
+    constexpr int screenHeight = 720;
     const std::string title = "SandboxGame";
 
-    glfwInit();
-    
-    // OpenGL 4.1 Core
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    InitWindow(screenWidth, screenHeight, title.c_str());
+    SetTargetFPS(60);
 
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
+    int frame = 0;
+    const char* message;
+    float image_rot = 0.f;
 
-    sg_desc desc = {0};
-    desc.environment = {
-        .defaults = {
-            .color_format = SG_PIXELFORMAT_RGBA8,
-            .depth_format = SG_PIXELFORMAT_DEPTH_STENCIL,
-            .sample_count = 1
-        }
+    Texture2D image = LoadTexture("opengl.png");
+
+    Camera2D camera = {
+        .offset = {
+            .x = screenWidth / 2,
+            .y = screenHeight / 2
+        },
+        .target = {
+            .x = screenWidth / 2,
+            .y = screenHeight / 2
+        },
+        .rotation = 0.0f,
+        .zoom = 1.0f
     };
 
-    desc.logger.func = basicLogger;
-
-    sg_setup(desc);
-
-    if (sg_isvalid()) {
-        std::println("SokolGfx: Setup was valid, continuing...");
-    }
-
-    sgl_desc_t gl_desc = {
-        .logger = basicLogger
-    };
-
-    sgl_setup(gl_desc);
-
-    sg_sampler_desc sampler_desc = {
-        .min_filter = SG_FILTER_LINEAR,
-        .mag_filter = SG_FILTER_LINEAR
-    };
-
-    sg_sampler sampler = sg_make_sampler(sampler_desc);
-
-   // simgui_desc_t imgui_desc = {
-   //     .logger = {
-   //         .func = basicLogger
-   //     }
-   // };
-
-   // simgui_setup(imgui_desc);
-
-    // font stuff
-
-    auto round_pow2 = [](float v) -> int {
-        uint32_t vi = ((uint32_t) v) - 1;
-        for (uint32_t i = 0; i < 5; i++) {
-            vi |= (vi >> (1<<i));
-        }
-        return (int) (vi + 1);
-    };
-
-    const int dim = round_pow2(1024.0f * dpi);
-
-    sfons_desc_t fons_desc = {
-        .width = 1024,
-        .height = 1024
-    };
-
-    fons = sfons_create(&fons_desc);
-    font = FONS_INVALID;
-
-    fonsSetErrorCallback(fons, [](void* uptr, int error, int val) {
-        std::println("FONS error: {}, value: {}", error, val);
-    }, nullptr);
-
-
-    sfetch_desc_t fetch_desc = {
-        .num_channels = 1,
-        .num_lanes = 4,
-        .logger = {
-            .func = basicLogger
-        }
-    };
-
-    sfetch_setup(fetch_desc);
-
-    char pathbuf[2048];
-
-    sfetch_request_t fetch_request = {
-        .path = fileutil_get_path("SourceCodePro-Regular.ttf", pathbuf, sizeof(pathbuf)),
-        .callback = onFontLoad,
-        .buffer = SFETCH_RANGE(font_data)
-    };
-
-    sfetch_send(fetch_request);
-
-    // images
-
-    int* file_buffer = (int*)std::malloc(256 * 1024);
-
-    sfetch_request_t image_fetch_request = {
-        .path = fileutil_get_path("opengl.png", pathbuf, sizeof(pathbuf)),
-        .callback = image_callback,
-        .buffer = SFETCH_RANGE(file_buffer)
-    };
-
-    sfetch_send(image_fetch_request);
-
-    sg_pass_action pass_action{};
-    pass_action.colors[0] = {
-        .load_action = SG_LOADACTION_CLEAR,
-        .clear_value = {0.18f, 0.21f, 0.5f, 1.0f}
-    };
-
-
-    sg_pass pass = {
-        .action = pass_action,
-        .swapchain = {
-            .width = width,
-            .height = height, 
-            .sample_count = 1,
-            .color_format = SG_PIXELFORMAT_RGBA8,
-            .depth_format = SG_PIXELFORMAT_DEPTH_STENCIL,
-            .gl = {
-                .framebuffer = 0
-            }
-        }
-    };
-
-    uint64_t frame = 0;
-
-    while (!glfwWindowShouldClose(window)) {
-        sfetch_dowork();
-        //simgui_new_frame({
-        //    width, 
-        //    height, 
-        //    1.f / 60.f, // TODO: Get actual frame time, rn im assuimg a constant 60fps
-        //    dpi
-       // });
-
-       // ImGui::ShowDemoWindow();
-        fonsClearState(fons);
-        
-        sgl_defaults();
-        sgl_matrix_mode_projection();
-
-        // Here is my understanding of glOrtho
-
-        /*
-        *
-        * near and far are z cutoff (all geom needs to be inbetween them, or else it won't be rendered)
-        * left and right, are minimum and maximum x 
-        * bottom and top is minimum and maximum y
-        */
-        sgl_ortho(0.0f, 1280.f, 720.f, 0.f, -1.0f, 1.0f); 
-        fonsSetFont(fons, font);
-        fonsSetSize(fons, 16.f);
-
-        constexpr float start_x = 375.f;
-        float x = start_x;
-        float y = 150.f;  
-
-        float line_height;
-        fonsVertMetrics(fons, nullptr, nullptr, &line_height);
-        
-        fonsSetColor(fons, sfons_rgba(255, 0, 0, 255));
-        x = fonsDrawText(fons, x, y, "red", nullptr);
-
-        fonsSetColor(fons, sfons_rgba(255, 165, 0, 255));
-        x = fonsDrawText(fons, x, y, "orange", nullptr);
-
-        fonsSetColor(fons, sfons_rgba(255, 255, 0, 255));
-        x = fonsDrawText(fons, x, y, "yellow", nullptr);
-
-        fonsSetColor(fons, sfons_rgba(0, 255, 0, 255));
-        x = fonsDrawText(fons, x, y, "green", nullptr);
-
-        fonsSetColor(fons, sfons_rgba(0, 0, 255, 255));
-        x = fonsDrawText(fons, x, y, "blue", nullptr);
-
-        fonsSetColor(fons, sfons_rgba(128, 0, 128, 255));
-        x = fonsDrawText(fons, x, y, "purple", nullptr);
-
-        fonsSetColor(fons, sfons_rgba(75, 0, 130, 255));
-        x = fonsDrawText(fons, x, y, "indigo", nullptr);
-
-        fonsSetColor(fons, sfons_rgba(238, 130, 238, 255));
-        x = fonsDrawText(fons, x, y, "violet", nullptr);
-
-        x = start_x; // reset to line start
-        y += line_height;
-
-        fonsSetColor(fons, sfons_rgba(255, 255, 255, 255));
-        x = fonsDrawText(fons, x, y, "this should be white text on another line", nullptr);
-
-        x = start_x; 
-        y += line_height;
-
-        fonsSetColor(fons, sfons_rgba(31, 31, 31, 255));
-        x = fonsDrawText(fons, x, y, "Line 1", nullptr);
-
-        x = start_x; 
-        y += line_height;
-
-        fonsSetColor(fons, sfons_rgba(63, 63, 63, 255));
-        x = fonsDrawText(fons, x, y, "Line 2", nullptr);
-
-        x = start_x; 
-        y += line_height;
-
-        fonsSetColor(fons, sfons_rgba(95, 95, 95, 255));
-        x = fonsDrawText(fons, x, y, "Line 3", nullptr);
-
-        x = start_x; 
-        y += line_height;
-
-        fonsSetColor(fons, sfons_rgba(127, 127, 127, 255));
-        x = fonsDrawText(fons, x, y, "Line 4", nullptr);
-
-        x = start_x; 
-        y += line_height;
-
-        fonsSetColor(fons, sfons_rgba(159, 159, 159, 255));
-        x = fonsDrawText(fons, x, y, "Line 5", nullptr);
-
-        x = start_x; 
-        y += line_height;
-
-        fonsSetColor(fons, sfons_rgba(191, 191, 191, 255));
-        x = fonsDrawText(fons, x, y, "Line 6", nullptr);
-
-        x = start_x; 
-        y += line_height;
-
-        fonsSetColor(fons, sfons_rgba(223, 223, 223, 255));
-        x = fonsDrawText(fons, x, y, "Line 7", nullptr);
-
-        x = start_x; 
-        y += line_height;
-
-        fonsSetColor(fons, sfons_rgba(255, 255, 255, 255));
-        x = fonsDrawText(fons, x, y, "Line 8", nullptr);
-
-        sfons_flush(fons);
-
-        sg_begin_pass(pass);
-
-        sgl_begin_triangles();
-
-        sgl_v2f_c3f(50.0f, 100.f, 1.0f, 0.0f, 0.0f);
-        sgl_v2f_c3f(100.f, 0.f, 0.0f, 1.0f, 0.0f);
-        sgl_v2f_c3f(0.f, 0.f, 0.0f, 0.0f, 1.0f);
-
-        sgl_end();
-
-        sgl_enable_texture();
-        sgl_texture(tex, sampler);
-
-        // sgl_begin_quads(); // Start drawing quads
-        // sgl_v2f_t2f(0.0f, 0.0f, 0.0f, 0.0f); // Bottom-left corner, UV (0,0)
-        // sgl_v2f_t2f(100.0f, 0.0f, 1.0f, 0.0f); // Bottom-right corner, UV (1,0)
-        // sgl_v2f_t2f(100.0f, 100.0f, 1.0f, 1.0f); // Top-right corner, UV (1,1)
-        // sgl_v2f_t2f(0.0f, 100.0f, 0.0f, 1.0f); // Top-left corner, UV (0,1)
-        // sgl_end();
-
-        sgl_draw();
-
-        //simgui_render();
-
-        sg_end_pass();
-        sg_commit();
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
+    while (!WindowShouldClose()) {
         frame++;
+        float delta = GetFrameTime();
+        float rot_speed = 100.f;
+        float move_speed = 2.f;
+
+        image_rot += 2.f * delta;
+
+        Vector2 scroll_vector = GetMouseWheelMoveV();
+
+        if (IsKeyDown(KEY_Q)) {
+            camera.rotation -= rot_speed * delta;
+        }
+
+        if (IsKeyDown(KEY_E)) {
+            camera.rotation += rot_speed * delta;
+        } 
+
+        if (scroll_vector.y != 0) {
+            camera.zoom += delta * scroll_vector.y;
+            camera.zoom = std::clamp(camera.zoom, 0.01f, 10.f);
+
+        }
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+            Vector2 mouse_delta = GetMouseDelta();
+            camera.target.x -= move_speed * mouse_delta.x;
+            camera.target.y -= move_speed * mouse_delta.y;
+        }
+        //std::println("{}, {}", scroll_vector.x, scroll_vector.y);
+
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        BeginMode2D(camera);
+            DrawRectangle(20, 20, 35, 35, RED);
+            DrawText(std::format("{}", GetFrameTime()).c_str(), 300, 0, 64, GREEN);
+            DrawTextureEx(image, {
+                static_cast<float>(screenWidth/2 - image.width/2),
+                static_cast<float>(screenHeight/2 - image.height/2)},
+                image_rot,
+                1.0f,
+                WHITE
+            );
+            DrawRectangleGradientV(1280, 720, 400, 400, BLACK, WHITE);
+        EndMode2D();       
+
+        EndDrawing();
     }
-
-    std::free(font_data);
-    std::free(file_buffer);
-
-    sfetch_shutdown();
-    sfons_destroy(fons);
-    sgl_shutdown();
-    //simgui_shutdown();
-    sg_shutdown();
-    glfwTerminate();
-
+    UnloadTexture(image);
+    CloseWindow();
     return 0;
-
 }
+
+
