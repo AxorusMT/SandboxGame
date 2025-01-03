6f73b111e11702f9eb92ab89e0359ea631779614
AxorusMT <ftmwini@gmail.com>
2025-01-03 00:22:02 +0000

    Playing around more with raylib

---
+++ Source/Engine/Base.hpp
@@ -0,0 +1,32 @@
+#include "raylib.h"
+
+// This header aims to "clean up" the raylib headers, removing anything we don't need,
+// making it easier to use within a C++ context.
+
+// We use our own colors, undefine the raylib macros
+#undef LIGHTGRAY  
+#undef GRAY       
+#undef DARKGRAY   
+#undef YELLOW     
+#undef GOLD       
+#undef ORANGE    
+#undef PINK       
+#undef RED        
+#undef MAROON     
+#undef GREEN      
+#undef LIME       
+#undef DARKGREEN  
+#undef SKYBLUE    
+#undef BLUE       
+#undef DARKBLUE   
+#undef PURPLE     
+#undef VIOLET     
+#undef DARKPURPLE 
+#undef BEIGE      
+#undef BROWN      
+#undef DARKBROWN  
+#undef WHITE      
+#undef BLACK      
+#undef BLANK      
+#undef MAGENTA    
+#undef RAYWHITE  
--- Source/Game/main.cpp
+++ Source/Game/main.cpp
@@ -19,7 +19,9 @@ int main() {
     const char* message;
     float image_rot = 0.f;
 
-    Texture2D image = LoadTexture("opengl.png");
+    Texture2D image = LoadTexture("grass.png");
+    Texture2D dirt = LoadTexture("dirt.png");
+    Texture2D error = LoadTexture("error.png");
 
     Camera2D camera = {
         .offset = {
@@ -34,13 +36,18 @@ int main() {
         .zoom = 1.0f
     };
 
+    int x = 0;
+    int y = (screenHeight/2 - image.height/2) - 35;
+    float rot_speed = 100.f;
+    float move_speed = 2.f;
+    float cube_speed = 500.f;
+    float my = 0.0f;
+
     while (!WindowShouldClose()) {
         frame++;
         float delta = GetFrameTime();
-        float rot_speed = 100.f;
-        float move_speed = 2.f;
 
-        image_rot += 2.f * delta;
+        image_rot += 20.f * delta;
 
         Vector2 scroll_vector = GetMouseWheelMoveV();
 
@@ -52,39 +59,79 @@ int main() {
             camera.rotation += rot_speed * delta;
         } 
 
-        if (scroll_vector.y != 0) {
-            camera.zoom += delta * scroll_vector.y;
-            camera.zoom = std::clamp(camera.zoom, 0.01f, 10.f);
+        if (IsKeyDown(KEY_RIGHT)) {
+            x += cube_speed * delta;
+        } else if (IsKeyDown(KEY_LEFT)) {
+            x -= cube_speed * delta;
+        }
 
+
+        if (scroll_vector.y != 0) {
+            float zoom_factor = 1.1f; // Adjust as needed for sensitivity
+            if (scroll_vector.y > 0) {
+                camera.zoom *= zoom_factor; // Zoom in
+            } else {
+                camera.zoom /= zoom_factor; // Zoom out
+            }
+            camera.zoom = std::clamp(camera.zoom, 0.01f, 10.f);
         }
 
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
             Vector2 mouse_delta = GetMouseDelta();
-            camera.target.x -= move_speed * mouse_delta.x;
-            camera.target.y -= move_speed * mouse_delta.y;
+            float adjusted_move_speed = move_speed / camera.zoom;
+            camera.target.x -= adjusted_move_speed * mouse_delta.x;
+            camera.target.y -= adjusted_move_speed * mouse_delta.y;
+            
         }
+        Vector2 mouse_pos = GetMousePosition();
+        Vector2 world_pos = GetScreenToWorld2D(mouse_pos, camera);
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+            x = world_pos.x;
+            my = world_pos.y;
+        }
+
+    
         //std::println("{}, {}", scroll_vector.x, scroll_vector.y);
 
         BeginDrawing();
 
-        ClearBackground(BLACK);
+        ClearBackground(SKYBLUE);
 
         BeginMode2D(camera);
-            DrawRectangle(20, 20, 35, 35, RED);
-            DrawText(std::format("{}", GetFrameTime()).c_str(), 300, 0, 64, GREEN);
-            DrawTextureEx(image, {
-                static_cast<float>(screenWidth/2 - image.width/2),
-                static_cast<float>(screenHeight/2 - image.height/2)},
-                image_rot,
-                1.0f,
-                WHITE
-            );
-            DrawRectangleGradientV(1280, 720, 400, 400, BLACK, WHITE);
-        EndMode2D();       
+            DrawRectangle(x, y, 35, 35, RED);
+            for (int i = -64 ; i < 63 ; i++) {
+                DrawTexture(image,
+                    static_cast<float>(screenWidth/2 - image.width/2) + (i * image.width),
+                    (screenHeight/2 - image.height/2),
+                    WHITE
+                );
+            }
+
+            for (int i = -64 ; i < 63 ; i++) {
+                for (int j = 0 ; j < 16 ; j++) {
+                    DrawTexture(dirt,   
+                        static_cast<float>(screenWidth/2 - dirt.width/2) + (i * dirt.width),
+                        (screenHeight/2 - dirt.height/2) + (dirt.height * (1 + j)),
+                        WHITE
+                    );
+                }
+            }
+            
+            DrawRectangle(0, -720 - ( std::sin(GetTime()) * 3000.f * delta), 400, 400, YELLOW);
+            DrawText(std::format("({:.2f}, {:.2f})", static_cast<float>(x) / 32.f, static_cast<float>(y) / 32.f).c_str(), x, y - 40, 30, BLACK);
+        EndMode2D(); 
+
+        DrawText(std::format("Goto ({:.2f}, {:.2f})", static_cast<float>(world_pos.x) / 32.f, static_cast<float>(world_pos.y) / 32.f).c_str(), 5, 40, 16, BLACK);
+        DrawText(std::format("Monitor: {}", GetMonitorName(GetCurrentMonitor())).c_str(), 0, screenHeight - 32, 32, BLACK);
+        DrawFPS(0,0);    
 
         EndDrawing();
     }
     UnloadTexture(image);
+    UnloadTexture(dirt);
+    UnloadTexture(error);
+
     CloseWindow();
     return 0;
 }
